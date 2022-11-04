e41aa6c22290a58bb5f40f540ca1ef84a850a2ed
felinna <felinakhasanah.051@gmailcom>
2022-11-04 14:48:06 +0700

    pertemuan 2

--- test/async.dart
+++ test/async.dart
@@ -1,48 +1,48 @@
-void main () async {
-  func1();
-  /*await func4(60).then((value) {
-    print(value);
-    print("then");
-    },
-  ).catchError((error) {
-    print(error);
-    print("error");
-  });*/
+// void main () async {
+//   func1();
+//   /*await func4(60).then((value) {
+//     print(value);
+//     print("then");
+//     },
+//   ).catchError((error) {
+//     print(error);
+//     print("error");
+//   });*/
   
-  try{
-    var value = await func4(50);
-    print (value);
-    print("then");
-  } catch(error){
-    print(error);
-    print("error");
-  }
+//   try{
+//     var value = await func4(50);
+//     print (value);
+//     print("then");
+//   } catch(error){
+//     print(error);
+//     print("error");
+//   }
   
-  func2();
-  func3(); 
-}
+//   func2();
+//   func3(); 
+// }
 
-func1(){
-  print("Function 1");
-}
+// func1(){
+//   print("Function 1");
+// }
 
-func2(){
-  print("Function 2");
-}
+// func2(){
+//   print("Function 2");
+// }
 
-func3(){
-  print("Function 3");
-}
+// func3(){
+//   print("Function 3");
+// }
 
-Future<String> func4(int nilai){  
-  return Future.delayed(Duration(seconds: 3), () {
+// Future<String> func4(int nilai){  
+//   return Future.delayed(Duration(seconds: 3), () {
     
-    if(nilai > 50){
-      return "Lulus";
-    }else {
-      throw "Error Tidak Lulus";
-    }
+//     if(nilai > 50){
+//       return "Lulus";
+//     }else {
+//       throw "Error Tidak Lulus";
+//     }
     
- });
-}
+//  });
+// }
 
